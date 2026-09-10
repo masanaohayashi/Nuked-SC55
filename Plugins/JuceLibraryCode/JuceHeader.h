76618d6ebce1667e0addcc0f5ffbd939b61c72d4
Masanao Hayashi <masanaohayashi@mac.com>
2026-09-11 02:15:49 +0900

    Update project and generated build versions to 0.0.6

--- Plugins/JuceLibraryCode/JuceHeader.h
+++ Plugins/JuceLibraryCode/JuceHeader.h
@@ -45,7 +45,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SC-55";
     const char* const  companyName    = "STUDIO-R";
-    const char* const  versionString  = "0.0.5";
-    const int          versionNumber  = 0x5;
+    const char* const  versionString  = "0.0.6";
+    const int          versionNumber  = 0x6;
 }
 #endif
